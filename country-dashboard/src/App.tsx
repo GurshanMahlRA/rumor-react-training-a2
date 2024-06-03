60ce6413b4b1e7e61b36a0cfa69494b814090c0e
Gurshanmahl <gurshanmahl@uvic.ca>
2024-06-02 17:53:51 -0700

    Completed Empty page

--- country-dashboard/src/App.tsx
+++ country-dashboard/src/App.tsx
@@ -3,8 +3,10 @@ import countries from './data/countries.json';
 import Currency from './Content/Currency';
 import Language from './Content/Language';
 import Region from './Content/Region';
-import Card from  './Content/Card';
+import Card from './Content/Card';
+import Empty from './Content/Empty';
 
+let test =0;
 
 export const App = () => {
   return (
@@ -18,25 +20,29 @@ export const App = () => {
         <Language />
         <Region />
       </nav>
-      <div className="cardDiv">
-        {countries.map((country, index) => {
-          const currencyKey = country.currencies ? Object.keys(country.currencies)[0] : '';
-          const currencyName = currencyKey;
-          const capital = country.capital ? country.capital[0] : 'Unknown';
+      {countries.length === 0 ? (
+          <Empty />
+      ) : (
+        <div className="cardDiv">
+          {countries.map((country, index) => {
+            const currencyKey = country.currencies ? Object.keys(country.currencies)[0] : '';
+            const currencyName = currencyKey;
+            const capital = country.capital ? country.capital[0] : 'Unknown';
 
-          return (
-            <Card
-              key={index}
-              name={country.name.common}
-              population={country.population}
-              region={country.region}
-              capital={capital}
-              currency={currencyName}
-              flagUrl={country.flags.png}
-            />
-          );
-        })}
-      </div>
+            return (
+              <Card
+                key={index}
+                name={country.name.common}
+                population={country.population}
+                region={country.region}
+                capital={capital}
+                currency={currencyName}
+                flagUrl={country.flags.png}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
---
+++ country-dashboard/src/Content/Empty.tsx
@@ -0,0 +1,13 @@
+export const Empty = () => {
+    return (
+      <div className="emptyDiv">
+        <div className="emptyDiv-content">
+          <h1>Nothing Found!</h1>
+          <button className="clear-filters">Clear Filters</button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Empty;
+  
